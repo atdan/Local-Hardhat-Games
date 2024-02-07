a8264a1faace3cc52bf0177818d1c1a38a6adf1f
atdan <atumadann@gmail.com>
2024-02-07 15:36:23 +0100

    update games 1 to 5 win.js to emit winner event

--- scripts/win5.js
+++ scripts/win5.js
@@ -1,12 +1,14 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
+const contractName = "Game5";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
+    await game.giveMeAllowance(20000);
+    await game.mint(10001);
     const tx = await game.win();
 
     // did you win? Check the transaction receipt!
